5e093827fd351c3baef3e150017b844308d171f7
Angela Diaz <angeldi846@gmail.com>
2022-12-05 17:18:08 -0500

    Creación de componente Footer y reemplazo de imágenes de logos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import {Holis} from './Holis'
-import Parrafo from './Parrafo'
-import {useState} from 'react'
 import "./App.css"
+import Footer from "./Footer"
 
 export default function App() {
-  let [count, setCount] = useState(0)
-  //let [x, setx] = useState(10)
-  const accion = _ => setCount(count + 1)
-  //const accion2 = _ => setx(x + 10)
-  return (<>
-    <Holis name="Dylan" num={count}/>
-    <Parrafo ex={accion}/>
-  </>)
+  return (
+    <>
+      <Footer />
+    </>
+  )
     
 }
 
