a29c48a9cca2af9e52a172b4314e7fba0563abca
ElPardo16 <andresito105@gmail.com>
2022-12-06 21:13:50 -0500

    componente graficos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Holis} from './Holis'
 import Parrafo from './Parrafo'
 import {useState} from 'react'
 import "./App.css"
+import Graph from './Graph'
 
 export default function App() {
   let [count, setCount] = useState(0)
@@ -9,8 +10,8 @@ export default function App() {
   const accion = _ => setCount(count + 1)
   //const accion2 = _ => setx(x + 10)
   return (<>
-    <Holis name="Dylan" num={count}/>
-    <Parrafo ex={accion}/>
+    <Graph coin="ethereum" type={0}/>
+    <Graph currency="usd" type={1}/>
   </>)
     
 }
