59c6e5b6d9c69eadacc50f72916fe5bbb96cabbd
ElPardo16 <andresito105@gmail.com>
2022-12-06 21:49:01 -0500

    merge alejo

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import "./card.css"
+
+export function Cards({porcentaje, price}){
+    return <div className="cont-cards">
+        <div className="card">
+            <div className="img"></div>
+            <h2 className="price">{price}</h2>
+            <h4 className="porcentajes">{porcentaje}</h4>
+        </div>
+
+    </div>
+}
